03dcd855503580853fb33925296d4887bcd985dc
“Nicole <nicolecondeduque@gmail.com>
2023-02-17 11:35:01 -0500

    inicio de pruebas del proyecto

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const path = require('path');
+
+const mdLinks = (path,options) => {
+  //resolve(resuelto) cuando se resuelve la promesa, y reject no, 
+  //relacionado al then y catch, resolve y reject son callback
+  //son funciones!! en resolve le damos el valor 
+  return new Promise((resolve,reject)=> {
+    //identificar si la ruta existe.
+    //si no existe la ruta rechaza la promesa.
+    if(fs.existsSync(path)) {
+   //Chequear o convertir a una ruta absoluta.
+   //Probar si va a ser archivo o directorio.
+   //Si es un directorio hay que devolver un arreglo con archivosque sean md
+   //Si es carpeta 
+    } else {
+       //Si no exite la ruta rechazamos la promesa.
+  reject('la ruta no existe')
+    }
+  });
+
+}
+
+module.exports = {
+  mdLinks
+
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+const { mdLinks } = require('../index.js');
 
 
 describe('mdLinks', () => {
@@ -6,5 +6,12 @@ describe('mdLinks', () => {
   it('should...', () => {
     console.log('FIX ME!');
   });
-
+  // it('Debería devolver una promesa', () => {
+  //   expect(mdLinks()).toBe(typeof Promise);
+  // });
+  it('Debería rechazar cuando el path no existe', () => {
+    return mdLinks('estepathnoexiste.md').catch((error)=>{
+    expect(error).toBe('la ruta no existe')
+    });
+  });
 });
